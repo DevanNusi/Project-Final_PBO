8641752b12dcdf73049d24d2930cdca48100ad82
Devan <2381012@unai.edu>
2024-12-06 12:15:03 +0600

    menambah module info

--- module-info.java
+++ module-info.java
@@ -1,2 +1,16 @@
-module Project.Final.PBO {
+module PertaCalcApp {
+    requires spring.context;
+    requires spring.beans;
+    requires java.sql;
+    requires org.slf4j;
+
+    opens PertaCalcApp;
+    opens PertaCalcApp.entities;
+    opens PertaCalcApp.config;
+    opens PertaCalcApp.views;
+    opens PertaCalcApp.services;
+    opens PertaCalcApp.repositories;
+
+
+
 }
